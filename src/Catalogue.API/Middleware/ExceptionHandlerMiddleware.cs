8f99cd3f0359530677bb643a22ff042fe1557fc6
Levan Kusiani <levani.kusiani@gmail.com>
2022-08-08 03:06:46 +0400

    refactored

---
+++ src/Catalogue.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mime;
+
+namespace Catalogue.API.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException ex)
+            {
+                _logger.LogError(new EventId(ex.HResult),
+                    ex,
+                    ex.Message);
+
+                var problemDetails = new ValidationProblemDetails()
+                {
+                    Instance = context.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = $"{ex.Message}: " + string.Join(", ", ex.Errors.Select(x => x.ErrorMessage))
+                };
+
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(problemDetails));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(new EventId(ex.HResult),
+                    ex,
+                    ex.Message);
+
+                var json = new JsonErrorResponse
+                {
+                    Messages = new[] { "An error occur.Try it again." },
+                    DeveloperMessage = ex
+                };
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(json));
+            }
+        }
+
+        private class JsonErrorResponse
+        {
+            public string[] Messages { get; set; }
+
+            public object DeveloperMessage { get; set; }
+        }
+    }
+}
---
+++ src/Catalogue.API/Program.cs
@@ -0,0 +1,68 @@
+using Catalogue.API.Middleware;
+using Catalogue.Application.Commands.ItemAggregate;
+using Catalogue.Application.Configurations;
+using Catalogue.Application.Queries.ItemQueries;
+using Catalogue.Domain.Entities.ItemAggregate;
+using Catalogue.Domain.SeedWork;
+using Catalogue.Infrastructure.Behaviours;
+using Catalogue.Infrastructure.Database;
+using Catalogue.Infrastructure.Repositories;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<CatalogueDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CatalogueDbConnection")
+        ?? throw new ArgumentNullException("CatalogueDbConnection")));
+
+//TODO: resolve this warning
+//using var serviceProvider = builder.Services.BuildServiceProvider();
+
+builder.Services.AddValidatorsFromAssemblyContaining<CreateItemCommandValidator>();
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+builder.Services.AddMediatR(typeof(CreateItemCommandHandler));
+
+builder.Services.AddOptions();
+builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection(nameof(ConnectionStrings)));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IItemQueries, ItemQueries>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+app.MapControllers();
+
+
+//using var scope = serviceProvider.CreateScope();
+//using var dbContext = scope.ServiceProvider.GetService<CatalogueDbContext>()
+//    ?? throw new ArgumentNullException(nameof(CatalogueDbContext));
+
+app.Run();
+using var dbContext = app.Services.GetRequiredService<CatalogueDbContext>()
+    ?? throw new ArgumentNullException(nameof(CatalogueDbContext));
+dbContext.Database.Migrate();
